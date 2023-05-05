8327d20ea0d8fe3ee2343e3fcdf28a6cc53280c1
Brenda <brentorres26@gmail.com>
2023-05-05 09:14:44 -0300

    Creacion de Clases Huespedes y Reservas

---
+++ hotelAlura/src/main/java/challenge/alura/hotelAlura/modelos/FormaDePago.java
@@ -0,0 +1,9 @@
+package challenge.alura.hotelAlura.modelos;
+
+public enum FormaDePago {
+
+	EFECTIVO,
+	TARJETA_DEBITO,
+	TARJETA_CREDITO,
+	TRANSFERENCIA
+}
---
+++ hotelAlura/src/main/java/challenge/alura/hotelAlura/modelos/Huesped.java
@@ -0,0 +1,12 @@
+package challenge.alura.hotelAlura.modelos;
+
+public class Huesped {
+
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String fecha_Nacimiento;
+	private String nacionalidad;
+	private String telefono;
+	private Reserva reserva;
+}
